393bd4e37f0ddf58d33694961e15f8508d032f8f
Suzuka Fukami <suzuka.fukami@societyone.com.au>
2021-11-14 16:53:13 +1100

    create new deck

--- deck.go
+++ deck.go
@@ -2,11 +2,23 @@ package main
 
 import "fmt"
 
-// Create new type of 'deck' which is a slice of strings
 type deck []string
 
-// receiver function: any vars of type deck have access to print() function
-// d is a reference of actual variable that'll be used for this function
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
